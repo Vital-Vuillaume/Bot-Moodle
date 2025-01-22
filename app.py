0f24968853823d55bb2a241ad6e75e7be23f2c2f
vuillaume <vitalvuillaume@proton.me>
2025-01-22 16:16:57 +0100

    opti

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
